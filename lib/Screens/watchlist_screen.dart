3b760c9d8883db326881e24924cde16d036783be
tobsq <tobsq@users.noreply.github.com>
2021-04-28 03:48:27 +0200

    V 1.04 - Watchlist zu kleinen Teil fertig

--- lib/Screens/watchlist_screen.dart
+++ lib/Screens/watchlist_screen.dart
@@ -1,15 +1,92 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WatchlistScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10),
-      width: MediaQuery.of(context).size.width,
-      color: Colors.black87,
-      child: Column(children: <Widget> [
-        Text('Watchlist')
-      ],),
-    );
+        width: MediaQuery.of(context).size.width,
+        color: Colors.black,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 15),
+                child: Text(
+                  'Watchlist',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: SizedBox(
+                      height: 50,
+                      child: TextField(
+                          decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.grey[500]),
+                              hintText: 'Search',
+                              prefix: Icon(Icons.search),
+                              fillColor: Colors.grey[900],
+                              filled: true,
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 0, style: BorderStyle.none),
+                                  borderRadius: BorderRadius.all(
+                                      Radius.circular(16))))))),
+              Container(
+                  height: 80,
+                  margin: EdgeInsets.only(top: 20, left: 15),
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      ScreenCategorys('images/bookmark.png', 'Watchlist'),
+                      ScreenCategorys('images/gorilla.png', 'Memestocks'),
+                      ScreenCategorys('images/fire.png', 'Hot'),
+                      ScreenCategorys('images/money.png', 'Indices'),
+                    ],
+                  ))
+            ],
+          ),
+        ));
   }
+}
+
+Widget ScreenCategorys(String image, String name) {
+  return Container(
+    margin: EdgeInsets.only(right: 15),
+    width: 80,
+    decoration: BoxDecoration(
+      color: Colors.grey[900],
+      borderRadius: BorderRadius.circular(18),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 8),
+          height: 40,
+          width: 40,
+          child: Image.asset(image),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 8),
+          child: Text(
+            name,
+            style: TextStyle(
+              color: Colors.grey[500],
+              fontSize: 11,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }
